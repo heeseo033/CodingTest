0dafd34efe8220149a5a34dfbe3a7baac9bcea55
heeseo033 <s2605@e-mirim.hs.kr>
2026-06-08 20:55:00 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n + 1) / 2];
+        int a = 0;
+        for(int i = 1; i <= n; i++)
+           if(i % 2 != 0)
+               answer[a++] = i;
+        return answer;
+    }
+}
