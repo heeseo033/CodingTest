5684dd76f8821c97e07b8f95cc003b8e00099d90
문희서 <heeseo040809@gmail.com>
2026-08-25 00:10:47 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] box, int n) {
+        int answer = 0;
+        answer += ((int)(box[0] / n)) * ((int)(box[1] / n)) * ((int)(box[2] / n));
+        
+        return answer;
+    }
+}
