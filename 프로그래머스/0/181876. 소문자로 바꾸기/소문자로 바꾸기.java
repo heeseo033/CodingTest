ca67c93d695b2e0eea4c7cb362bf60453619e7e0
문희서 <heeseo040809@gmail.com>
2026-08-27 08:00:02 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 3.28 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i = 0; i < myString.length(); i++){
+            char c = myString.charAt(i);
+            if(c >= 'A' && c <= 'Z'){
+                answer += (char)(c + 32);
+            }
+            else{
+                answer += c;
+            }
+        }
+        return answer;
+    }
+}
